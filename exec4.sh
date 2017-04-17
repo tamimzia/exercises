15949cdf9e357f6f6665950fac715e1eae06997f
Ahmad Tamim Zia <tamimzia@hotmail.de>
2017-04-17 18:43:58 +0200

    Create exec4.sh

---
+++ exec4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+### filename: exec4.sh
+### Linux Shell Scripting Training - Ahmad Tamim Zia
+### Exercise 4
+clear
+
+echo "expr 2 + 2"
+expr 2 + 2
+
+echo "expr 2 + 2 * 4"
+expr 2 + 2 \* 4
+
+echo "expr (2 + 2) * 4"
+expr \( 2 + 2 \) \* 4
