536d3c98318a181019ea806a705d07ce9cfd2f12
Ahmad Tamim Zia <tamimzia@hotmail.de>
2017-04-17 17:40:08 +0200

    Create exec1.sh

---
+++ exec1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+### exec1.sh
+### Linux Shell Scripting Training
+### Exercise 1
+
+STARTOFSCRIPT=` date +'%Y-%m-%d %H:%M:%S'` 
+MYUSERNAME="Tamin Zia"
+MYPASSWORD="Apassword123"
+clear
+echo "############################################"
+echo "Script stated at: $STARTOFSCRIPT"
+
+echo "My Name is:" $MYUSERNAME
+echo "Dummy Password:" $MYPASSWORD
+sleep 4
+ENDOFSCRIPT=` date +'%Y-%m-%d %H:%M:%S'` 
+echo "Script finished at: $ENDOFSCRIPT"
+
