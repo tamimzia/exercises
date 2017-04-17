b570d606d497898f22af1e01f7d7c6055f210447
Ahmad Tamim Zia <tamimzia@hotmail.de>
2017-04-17 17:58:34 +0200

    Create exec2.sh

---
+++ exec2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### exec2.sh
+### Linux Shell Scripting Training
+### Exercise 2
+clear
+echo "this text will be shown in the terminal window"
+echo "this text will NOT be shown in the terminal window" >> /dev/null
+echo "we  try to execute a command that does not exist, this will show a error message -->"
+command-not-found
+
+echo "we  try to execute a command that does not exist, but redirect error message -->"
+command-not-found &2> /dev/null
